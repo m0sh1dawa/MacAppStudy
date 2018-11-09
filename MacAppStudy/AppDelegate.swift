45a556d86b39efafdf32b6651eacaf75c25267bd
shimshimkaz <37267539+shimshimkaz@users.noreply.github.com>
2018-11-09 21:09:41 +0900

    Initial Commit

---
+++ MacAppStudy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacAppStudy
+//
+//  Created by kazuhiko.shimada on 2018/11/09.
+//  Copyright © 2018 kazuhiko.shimada. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacAppStudy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacAppStudy
+//
+//  Created by kazuhiko.shimada on 2018/11/09.
+//  Copyright © 2018 kazuhiko.shimada. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
