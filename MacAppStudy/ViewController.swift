cb46115ed9fe014d82d471211136f9e328a344b1
shimshimkaz <37267539+shimshimkaz@users.noreply.github.com>
2018-11-09 21:42:57 +0900

    [update] select and show image

--- MacAppStudy/ViewController.swift
+++ MacAppStudy/ViewController.swift
@@ -10,6 +10,23 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var imageView: NSImageView!
+    
+    @IBAction func imageSelectButtonClicked(_ sender: Any) {
+        let dialog = NSOpenPanel()
+        dialog.canChooseDirectories=false
+        dialog.canChooseFiles = true
+        dialog.canCreateDirectories = false
+        dialog.allowsMultipleSelection = false
+        dialog.allowedFileTypes = NSImage.imageTypes
+        dialog.begin { (result) -> Void in
+            if result.rawValue == NSApplication.ModalResponse.OK.rawValue {
+                guard dialog.url != nil else { return }
+                self.imageView.image = NSImage(contentsOf: dialog.url!)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
